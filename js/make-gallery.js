1e3e8a13212a4c88a1461e977767b45a3e070658
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-03-04 20:24:32 +0200

    add slider and modal close on press Esc or press Esc
    - add modal close on press Esc
    - add modal close on click outside
    - add slider

--- js/make-gallery.js
+++ js/make-gallery.js
@@ -5,6 +5,7 @@ const galleryRef = document.querySelector('.js-gallery');
 const ligthboxRef = document.querySelector('.lightbox');
 const lightboxImageRef = document.querySelector('.lightbox__image');
 const closeBtn = document.querySelector('[data-action="close-lightbox"]');
+const backdropRef = document.querySelector('.lightbox__overlay');
 
 const galleryCardsMurkup = makeCardsGallery(galleryImages);
 galleryRef.insertAdjacentHTML('afterbegin', galleryCardsMurkup);
@@ -12,6 +13,19 @@ galleryRef.insertAdjacentHTML('afterbegin', galleryCardsMurkup);
 galleryRef.addEventListener('click', onImageClick);
 
 closeBtn.addEventListener('click', onCloseModal);
+
+backdropRef.addEventListener('click', onBackdropClick);
+
+const arrayOfImageLinks = makeArrayOfImageLinks(galleryImages);
+const arrayOfImageDescriptions = makeArrayOfImageDescriptions(galleryImages);
+
+function makeArrayOfImageLinks(gallery) {
+    return gallery.map(({ original }) => original);
+}
+
+function makeArrayOfImageDescriptions(gallery) {
+    return gallery.map(({ description }) => description);
+}
 
 function makeCardsGallery (gallery) {
     return gallery.map(({ preview, original, description }) => {
@@ -46,16 +60,60 @@ function onImageClick(event) {
     onOpenModal(url, description);
 }
 
-function onOpenModal(url, description) {
-    ligthboxRef.classList.add('is-open');
+let position = 0;
 
+function onOpenModal(url, description) {
+    window.addEventListener('keydown', onEscKeyPress);
+    window.addEventListener('keydown', onArrowRightKeyPress);
+    window.addEventListener('keydown', onArrowLeftKeyPress);
+
+    ligthboxRef.classList.add('is-open');
     lightboxImageRef.setAttribute("src", url);
     lightboxImageRef.setAttribute("alt", description);
+
+    position = arrayOfImageLinks.indexOf(url);
+
+    return position;
+}
+
+function onArrowRightKeyPress(event) {
+    if (event.code === 'ArrowRight') {
+        position < 8 ? position += 1 : position = 0;
+
+        lightboxImageRef.setAttribute("src", arrayOfImageLinks[position]);
+        lightboxImageRef.setAttribute("alt", arrayOfImageDescriptions[position]);
+    }
+}
+
+function onArrowLeftKeyPress(event) {
+    if (event.code === 'ArrowLeft') {
+        position > 0 ? position -= 1 : position = 8;
+
+        lightboxImageRef.setAttribute("src", arrayOfImageLinks[position]);
+        lightboxImageRef.setAttribute("alt", arrayOfImageDescriptions[position]);
+    }
 }
 
 function onCloseModal() {
-    ligthboxRef.classList.remove('is-open');
+    window.removeEventListener('keydown', onEscKeyPress);
+    window.removeEventListener('keydown', onArrowRightKeyPress);
+    window.removeEventListener('keydown', onArrowLeftKeyPress);
 
+    ligthboxRef.classList.remove('is-open');
     lightboxImageRef.removeAttribute("src");
     lightboxImageRef.removeAttribute("alt");
+}
+
+function onBackdropClick(event) {
+    if (!event.target.classList.contains('lightbox__overlay')) {
+        return;
+    }
+    
+    onCloseModal();
+}
+
+function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+        onCloseModal();
+    }
 }
